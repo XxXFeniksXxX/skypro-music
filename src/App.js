3d12533529687f0fb8a96902de9a62aef615bf62
XxXFeniksXxX <kvazar_20letie@mail.ru>
2023-10-02 20:12:46 +0400

    react-baza

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='title'>
+          Здесь скоро будет Skypro.Music
         </p>
         <a
           className="App-link"
